185ecf92d6fa28d8f95085b2441df4a724c1db89
Darla David <201910063@feualabang.edu.ph>
2022-02-24 22:58:13 +0800

    Copy worked but first word only.

--- jejebackend.php
+++ jejebackend.php
@@ -56,26 +56,22 @@
                 echo "Made by gnd";
             }
         }
+
+        echo "<input type='text' name='mytext' id='mytext' value=$val>";
     ?>
     </form>
 
+    <button type="button" onclick='myFunction()'>Copy text</button>
 
-<!-- The button used to copy the text -->
-<button value=<?php $val?> id="myInput" onclick="myFunction()">Copy text</button>
 <script>
     function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("myInput");
-
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    var mytext = document.getElementById("mytext");	
+	
+    mytext.select(); //select text field
+    
+    document.execCommand("copy");  //Copy text
+    
+    document.getElementById("msg").innerHTML = "Copied";
     }
 </script>
 </body>

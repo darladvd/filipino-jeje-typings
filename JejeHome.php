f11a1993afd5777ab5c4dad2d3032aea14e44995
mrynlws <80335563+mrynlws@users.noreply.github.com>
2022-02-24 23:17:54 +0800

    Home Page HTML/CSS/PHP

---
+++ JejeHome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Jeje Typings Generator</title>
+ <meta charset="utf-8">
+ <link rel="stylesheet" type="text/css" href="JejeHome.css">
+ </head>
+ <body>
+		<img src='projectlogo.png' class='jejelogo' alt='Jeje Typings Logo'>
+    <center><button name="s" type="submit" class="generatebtn">Generate</button>
+    <br><br><br>
+		<form action="JejeHome.php" method="post">
+		<input type="text" name="textbox" value="The Jeje Typings will be generated here.">
+  </form>
+		<br><br><img src='companylogo.png' class='companylogo' alt='GND Company Logo'>
+ </body>
+ </html>
